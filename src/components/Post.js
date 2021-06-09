c3fcae540bd4b29c1d3e06c9b7a699cceb5b0696
hs_team <svekariy@rhrk.uni-kl.de>
2021-06-09 14:42:40 +0200

    image alt added

--- src/components/Post.js
+++ src/components/Post.js
@@ -8,7 +8,11 @@ const Post = ({ article }) => {
     <div className='post'>
       <h2 className='title'>{name}</h2>
       {featuredImage && (
-        <img className='featuredImage' src={featuredImage.fields.file.url} />
+        <img
+          className='featuredImage'
+          alt={name}
+          src={featuredImage.fields.file.url}
+        />
       )}
       <section dangerouslySetInnerHTML={{ __html: postDescription }} />
     </div>
